5d1f60c239435ba570ff6450f91ec0f46be98492
Tyler <tylerfromgw@gmail.com>
2022-07-09 20:29:47 -0500

    v2.0

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 const axios = require('axios');
 class Filter {
+    
+    
+    // purgomalum-filter: js swear lib
+    
     constructor(config={
-        fill_text: undefined,
         fill_char: undefined,
+        fill_text: undefined,
         add: []
-    }){
-        this.config=config
+    }) {
+        this.config = config;
     }
     
     async clean(text) {
         var send = this.config;
         send.text = text;
-        var response = await axios.get('https://www.purgomalum.com/service/json', { params: send });
-        response = response.data;
         
-        // handle
+        var response = axios.get('https://www.purgomalum.com/service/json', { params: send });
+        response = response.data;
         
         if (response.hasOwnProperty("result")) return response.result;
         else if (response.hasOwnProperty("error")) throw new Error(response.error);
-        else throw new Error("Unexpected server response.");
+        else throw new Error("Unexpected Server Response");
     }
     
     async containsProfanity(text) {
-        var send = {text:text, add:this.config.add}
-        var response = axios.get('https://www.purgomalum.com/service/containsprofanity', { params: send })
+        var send = { text: text, add: this.config.add }
+        var response = axios.get('https://www.purgomalum.com/service/containsprofanity', { params: send });
         response = response.data;
         return (response);
     }
+    
 }
 
 module.exports = Filter;
